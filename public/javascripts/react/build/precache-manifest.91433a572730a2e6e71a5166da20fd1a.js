2d2c50b3972a291f5629457d74db8b816f7b4f1d
Vanderson <vanderson.th@gmail.com>
2020-02-01 23:23:01 +0000

    add site funcional

---
+++ public/javascripts/react/build/precache-manifest.91433a572730a2e6e71a5166da20fd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64aa263aeb715a22f08b746b5cccc6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "736a6dcec875dac1d03e",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "74be88b4374aeb456550",
+    "url": "/static/css/main.90c42152.chunk.css"
+  },
+  {
+    "revision": "736a6dcec875dac1d03e",
+    "url": "/static/js/2.05343954.chunk.js"
+  },
+  {
+    "revision": "74be88b4374aeb456550",
+    "url": "/static/js/main.f1860d2e.chunk.js"
+  },
+  {
+    "revision": "19d590042be05d94698e",
+    "url": "/static/js/runtime-main.9bf3308e.js"
+  }
+]);
--- public/javascripts/react/build/service-worker.js
+++ public/javascripts/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8b9e573e7e5e6f11278ec1fa7069727.js"
+  "/precache-manifest.91433a572730a2e6e71a5166da20fd1a.js"
 );
 
 self.addEventListener('message', (event) => {
